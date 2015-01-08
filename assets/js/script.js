4798f26da028af90cd924e2be95937eb0203b9b7
Laurent MOREL <laurent.morel30@gmail.com>
2015-01-08 17:34:12 +0100

    Animation + "prérequis"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){  
+jQuery(document).ready(function($){
 
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
@@ -10,26 +10,42 @@ jQuery(document).ready(function($){
     });
 
     //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top-40
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top-40
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
-
 
     var margin_top = $(window).height() - (100 + $('#jumbo').height());
     $('#requirements').css('margin-top', margin_top);
-    console.log(margin_top);
-
 
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
 
-  $('.dropdown-toggle').dropdown()
+  $('.dropdown-toggle').dropdown();
 
- 
+  $('.cliquable_list ul > li').click(function(e){
+	var p = $($(this).children('p')[0]).children('span.not_shown')[0];
+
+	if( $(p).is(':visible') ){ $(p).fadeOut(); }
+	else{ $(p).fadeIn();  }
+  });
+
+  $rocket = new Object();
+
+  var timeoutID;
+  function endAnim() { $rocket.attr("class", ""); window.clearTimeout(timeoutID); }
+
+  $('#jumbo .visual').mouseenter(function(){
+        $rocket.attr("class", "animate").delay(1200);
+        timeoutID = window.setTimeout(endAnim, 1200);
+  });
+
+  $(window).on("load", function(){
+        $rocket = $('g#rocket-maskgroup');
+        $rocket.attr("class", "loadAnim");
+  });
+
 });
