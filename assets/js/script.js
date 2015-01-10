9c321fe013335beba08fea03ca585b7f4b77b58b
Laurent MOREL <laurent.morel30@gmail.com>
2015-01-10 12:40:27 +0100

    Fix + Examples

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 
-    //jQuery to collapse the navbar on scroll
+    /* .. */
     $(window).scroll(function() {
         if ($(".navbar").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -9,43 +9,48 @@ jQuery(document).ready(function($){
         }
     });
 
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    /* PAGE SCROLLING */
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-40
+            scrollTop: $($anchor.attr('href')).offset().top-100
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 
-    var margin_top = $(window).height() - (100 + $('#jumbo').height());
+    /* MARGIN */
+    var margin_top = $(window).height() - (90 + $('#jumbo').height() + $('#definition').height());
     $('#requirements').css('margin-top', margin_top);
+    $('#example').css('margin-top', margin_top-90);
 
-  $('.bs-component [data-toggle="popover"]').popover();
-  $('.bs-component [data-toggle="tooltip"]').tooltip();
+    /* BASE */
+    $('.bs-component [data-toggle="popover"]').popover();
+    $('.bs-component [data-toggle="tooltip"]').tooltip();
+    $('.dropdown-toggle').dropdown();
 
-  $('.dropdown-toggle').dropdown();
 
-  $('.cliquable_list ul > li').click(function(e){
-	var p = $($(this).children('p')[0]).children('span.not_shown')[0];
+    /* HIDDEN ELEMENTS */
+    $('.cliquable_list ul > li').click(function(e){
+        var p = $($(this).children('p')[0]).children('span.not_shown')[0];
 
-	if( $(p).is(':visible') ){ $(p).fadeOut(); }
-	else{ $(p).fadeIn();  }
-  });
+        if( $(p).is(':visible') ){ $(p).fadeOut(); }
+        else{ $(p).fadeIn();  }
+    });
 
-  $rocket = new Object();
 
-  var timeoutID;
-  function endAnim() { $rocket.attr("class", ""); window.clearTimeout(timeoutID); }
+    /* ANIMATION */
+    $rocket = new Object();
+    var timeoutID;
+    function endAnim() { $rocket.attr("class", ""); window.clearTimeout(timeoutID); }
 
-  $('#jumbo .visual').mouseenter(function(){
+    $('#jumbo .visual').mouseenter(function(){
         $rocket.attr("class", "animate").delay(1200);
         timeoutID = window.setTimeout(endAnim, 1200);
-  });
+    });
 
-  $(window).on("load", function(){
+    $(window).on("load", function(){
         $rocket = $('g#rocket-maskgroup');
         $rocket.attr("class", "loadAnim");
-  });
+    });
 
 });
